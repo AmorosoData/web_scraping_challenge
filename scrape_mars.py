21a5c248a696874681fea1570d9b160149b54585
AmorosoData <amoroson@gmail.com>
2020-09-23 12:38:47 -0700

    adding scrape

---
+++ scrape_mars.py
@@ -0,0 +1,11 @@
+# Importing dependencies
+from bs4 import BeautifulSoup
+from splinter import Browser
+from pprint import pprint
+import pymongo
+import pandas as pd
+import requests
+from flask import Flask, render_template
+import time
+import numpy as np
+import json
